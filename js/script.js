5ccdff883157f73f5d0c0d4b056e20801dd746df
James Fehr <fehr.james@gmail.com>
2023-02-06 12:38:24 -0800

    updates

--- js/script.js
+++ js/script.js
@@ -14,3 +14,14 @@ const na_l = document.getElementById(`nav-list`);
 const na_i = document.querySelector(`nav-item`);
 console.log(nav1);
 
+// if (nav1.style.display !== "flex"){
+//     nav1.style.display = "flex";
+//     nav1.style.animationName = "slide-in";
+//     return;
+// }
+// nav1.style.animationName = "fade-out";
+// setTimeout(() => {
+//     nav1.style.display = "none";
+//     console.log(nav.style.animationName);
+// }, 1000)
+
