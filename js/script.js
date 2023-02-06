3dbe6014c5820fb05633b34c3eb7578a3b711c89
James Fehr <fehr.james@gmail.com>
2023-02-06 12:57:08 -0800

    updates

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 const nav1 = document.querySelector(`nav`);
 const nav_b = document.getElementById(`nav-btn`);
 nav_b.onclick = () => {
-    if (nav1.style.display == "flex"){
-        nav1.style.display = "none";
-        return;
-    }  
-    console.log(nav1);
-    nav1.style.display = "flex";
-}
-
+  if (nav1.style.display == "flex") {
+    nav1.style.display = "none";
+    return;
+  }
+  console.log(nav1);
+  nav1.style.display = "flex";
+};
 
 const na_l = document.getElementById(`nav-list`);
 const na_i = document.querySelector(`nav-item`);
@@ -24,4 +23,3 @@ console.log(nav1);
 //     nav1.style.display = "none";
 //     console.log(nav.style.animationName);
 // }, 1000)
-
