36f8bd81a3fd981bc5430786729b39218b0d0b63
James Fehr <fehr.james@gmail.com>
2023-02-06 12:08:04 -0800

    fixing functionality of JS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-const nav_b = document.getElementById(`nav-btn`).onclick = () => {
-    // nav_b.style.backgroundColor="bluegreen";
+const nav1 = document.querySelector(`nav`);
+const nav_b = document.getElementById(`nav-btn`);
+nav_b.onclick = () => {
+    if (nav1.style.display == "flex"){
+        nav1.style.display = "none";
+        return;
+    }  
+    console.log(nav1);
+    nav1.style.display = "flex";
 }
-const nav1 = documnent.querySelector(`nav`).onclick = () => {
-    if (nav1.style.display === "none") {
-        nav1.style.display = "flex";
-    }
-}
+
+
+const na_l = document.getElementById(`nav-list`);
+const na_i = document.querySelector(`nav-item`);
+console.log(nav1);
+
