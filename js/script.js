1c7de4b9403a3410d4322196152cbe83983e51c7
James Fehr <fehr.james@gmail.com>
2023-02-06 09:58:47 -0800

    working on the js for the menu

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const nav_b = document.getElementById(`nav-btn`).onclick = () => {
+    // nav_b.style.backgroundColor="bluegreen";
+}
+const nav1 = documnent.querySelector(`nav`).onclick = () => {
+    if (nav1.style.display === "none") {
+        nav1.style.display = "flex";
+    }
+}
